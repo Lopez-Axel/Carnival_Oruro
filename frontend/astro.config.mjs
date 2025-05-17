f43efe7992f501b55e7dcdbd6dbbc3cd114f0e6a
Axel <axel.lopez.rol@gmail.com>
2025-05-17 16:23:12 -0400

    Modificacion del Astro - Astro Config JS

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-
   vite: {
     plugins: [tailwindcss()],
+    server: {
+      allowedHosts: ['carnival-oruro-astro.onrender.com']
+    }
   },
 });
